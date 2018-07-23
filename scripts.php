e216345199648223df6e9aa597f46f89cb82df76
ynswtf <senemyunus@gmail.com>
2018-07-23 22:55:48 +0300

    Add Image

--- scripts.php
+++ scripts.php
@@ -2,17 +2,12 @@
 return [
 
     'install' => function ($app) {},
-    'uninstall' => function ($app) {},
-    'enable' => function ($app) {},
-    'disable' => function ($app) {
-      $app['config']->remove('pastheme/anatolia');
+    'uninstall' => function ($app) {
+        $app['config']->remove('pastheme/anatolia');
     },
-    'updates' => [
-
-        '0.5.0' => function ($app) {},
-        '0.9.0' => function ($app) {}
-
-    ]
+    'enable' => function ($app) {},
+    'disable' => function ($app) {},
+    'updates' => []
 ];
 
 ?>
